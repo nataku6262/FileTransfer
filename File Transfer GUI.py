43d45fc709749ed1626f15e55ec46ab9157562aa
Gavin White <nataku6262@hotmail.co.uk>
2016-09-04 17:51:08 +0100

    File Transfer GUI.py is the completed 1.0 script that takes the input in a GUI and passes that to the File Transfer function successfully. Additional scripts are included that were created to test small areas of functionality to ensure that they are working. Will now be archived.

---
+++ File Transfer GUI.py
@@ -0,0 +1,125 @@
+import tkinter as tk
+from tkinter import ttk
+import os, shutil
+
+##Sort Function##
+
+def bubble(bad_list):
+    length = len(bad_list) - 1
+    sorted = False
+
+    while not sorted:
+        sorted = True
+        for i in range(length):
+            if bad_list[i] > bad_list[i+1]:
+                sorted = False
+                bad_list[i], bad_list[i+1] = bad_list[i+1], bad_list[i]
+
+##File Transfer Function##
+
+def File_Transfer():
+    folder = os.listdir(directory.get())
+    source = directory.get()
+    newFolder = newLocation.get()
+    fileName = newName.get()
+    extn = fileExtension.get()
+
+    S = []
+    for L in folder:
+        S.append(L)
+    bubble(S)
+
+    n = 1
+    for i in S:
+        if n < 10:
+            name = fileName[:-1]+str(n) + '.' + str(extn)
+        else:
+            name = fileName[:-2]+str(n) + '.' + str(extn)
+
+        shutil.copy (str(source) + '\\' + str(i), newFolder + '\\' + name)
+        n += 1
+
+    popup=tk.Tk()
+    popup.wm_title ("In Progress")
+    label = ttk.Label(popup, text ='Trasferring files...', font=FONT)
+    label.pack()
+    B1 = ttk.Button (popup, text='Ok', command = popup.destroy)
+    B1.pack()
+    popup.mainloop()
+
+
+#######GUI########
+####Class Framework####
+FONT = ('Calibri', 10)
+LARGE_FONT = ('Calibri', 12)
+
+class FileTransfer(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        tk.Tk.wm_title (self, 'File Transfer') #Window Title
+
+        container = tk.Frame(self)	
+        container.pack(side='top', fill='both', expand = True)
+        container.grid_rowconfigure (0, weight=1) 
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {} 
+
+        frame = StartPage (container, self)
+
+        self.frames[StartPage] = frame
+
+        frame.grid (row=0, column=0, sticky='nsew')
+
+        self.show_frame(StartPage)
+
+    def show_frame(self, cont):
+
+        frame= self.frames[cont]
+        frame.tkraise()
+
+class StartPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)        
+            
+        label = ttk.Label (self, text='Start Page', font=LARGE_FONT)
+        label.grid(row= 1, column=1, pady=10, padx=10)
+
+        directory_Label = ttk.Label (self, text='Directory Path', font=FONT)
+        directory_Label.grid (row=2, column=1, columnspan=1)
+
+        global directory
+        directory = ttk.Entry(self)
+        directory.grid(row=2, column=2, columnspan=2)
+
+        newLocation_Label = ttk.Label (self, text='New Location', font=FONT)
+        newLocation_Label.grid (row=3, column=1, columnspan=1, padx=5, pady=5)
+
+        global newLocation
+        newLocation = ttk.Entry(self)
+        newLocation.grid(row=3, column=2, columnspan=1, padx=5, pady=5)
+
+        newName_Label = ttk.Label (self, text='New Name', font=FONT)
+        newName_Label.grid (row=4, column=1, columnspan=1, padx=5, pady=5)
+
+        global newName
+        newName = ttk.Entry(self)
+        newName.grid(row=4, column=2, columnspan=1, padx=5, pady=5)
+
+        fileExtension_Label = ttk.Label (self, text='File Extension', font=FONT)
+        fileExtension_Label.grid (row=5, column=1, columnspan=1)
+
+        global fileExtension
+        fileExtension = ttk.Entry(self)
+        fileExtension.grid(row=5, column=2, columnspan=2)
+
+        submit = ttk.Button (self, text='Submit', command = File_Transfer)
+        submit.grid(row=6, column=2, pady = 15)
+
+
+app = FileTransfer()
+app.mainloop()
